66e63ff9eac678ed42479c5ccb36647c1a9aee9e
Dmitry Zhuk <dmitryzhuk98@gmail.com>
2018-11-07 18:51:30 +0300

    Initial commit

---
+++ app/src/main/java/com/github/zhukdi/github_fetch/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.github.zhukdi.github_fetch
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
